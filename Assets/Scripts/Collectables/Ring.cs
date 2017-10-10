e046abba4bb884ca6903c8b1d5eb1f6924e1757c
Michael Witkovsky <michael.witkovsky@gmail.com>
2017-10-10 14:25:18 -0400

    final?

---
+++ Assets/Scripts/Collectables/Ring.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            //More logic like adding to a score counter or something...
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Environment/MovingPlatform2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform2 : MonoBehaviour {
+
+    public float moveSpeed;
+    public Vector3 targetOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
